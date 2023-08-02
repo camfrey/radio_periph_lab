4ff47e21789bf7c882b9fe2fc5f2fff8d8c2b0d1
Cameron Frey <camfrey@protonmail.com>
2023-08-01 20:31:07 -0400

    Update radio test software

--- src/linux_software/test_radio.c
+++ src/linux_software/test_radio.c
@@ -61,9 +61,10 @@ void print_benchmark(volatile unsigned int *periph_base)
     float throughput=0; 
     // please insert your code here for calculate the actual throughput in Mbytes/second
     // how much data was transferred? How long did it take?
-    unsigned int bytes_transferred = 0; // change obviously
-    float time_spent = 1; // change obviously
-    printf("You transferred %f bytes of data in %f seconds\n",bytes_transferred,time_spent);
+    unsigned int bytes_transferred = 4 * 2048; // 2048 integers read, 4 bytes each
+    float time_spent = (float)(stop_time - start_time) * (float)1/125000000; // get number of clock cycles then multiply by the period of the clock
+    throughput = (float)(bytes_transferred / time_spent)/1e6;
+    printf("You transferred %d bytes of data in %f seconds\n",bytes_transferred,time_spent);
     printf("Measured Transfer throughput = %f Mbytes/sec\n",throughput);
 }
 
@@ -73,12 +74,13 @@ int main()
 // first, get a pointer to the peripheral base address using /dev/mem and the function mmap
     volatile unsigned int *my_periph = get_a_pointer(RADIO_PERIPH_ADDRESS);	
 
-    printf("\r\n\r\n\r\nLab 6 YOURNAME - Custom Peripheral Demonstration\n\r");
-    *(my_periph+RADIO_TUNER_CONTROL_REG_OFFSET) = 0; // make sure radio isn't in reset
+    printf("\r\n\r\n\r\nLab 6 Cameron Frey - Custom Peripheral Demonstration\n\r");
+    *(my_periph+RADIO_TUNER_CONTROL_REG_OFFSET) = 1; // active low reset change
     printf("Tuning Radio to 30MHz\n\r");
     radioTuner_tuneRadio(my_periph,30e6);
     printf("Playing Tune at near 30MHz\r\n");
     play_tune(my_periph,30e6);
     print_benchmark(my_periph);
+    *(my_periph+RADIO_TUNER_CONTROL_REG_OFFSET) = 0; // put radio in reset
     return 0;
 }
